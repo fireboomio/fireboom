f4d99fd73a17a42c986c518a34004c5335d9eb67
xufeixiang <xufeixiang@ainanjing.org.cn>
2024-07-17 17:34:08 +0800

    fix: resolve skip-front.sh not judge uname is Darwin or others.

--- scripts/skip-front.sh
+++ scripts/skip-front.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 INSERT_CHAR="/"
 ASSET_FILEPATH="assets/asset.go"
-if [ "$(uname -s)" == "Darwin" ];then
+if [ "$(uname -s)" = "Darwin" ];then
   sed -i '' "10s|^|${INSERT_CHAR}|" "$ASSET_FILEPATH"
   sed -i '' "13s|^|${INSERT_CHAR}|" "$ASSET_FILEPATH"
 else
