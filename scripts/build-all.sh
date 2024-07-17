7f59821afb5d2c78eb03dcfa428df2d847e6385a
erguotou <erguotou525@gmail.com>
2024-07-17 15:42:21 +0800

    ci: Add without-web version

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -4,9 +4,9 @@ mkdir -p release
 echo "$datetime" > release/build_time
 export FB_COMMIT=$(git rev-parse --short HEAD)
 
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w -X main.FbVersion=$FB_VERSION -X main.FbCommit=$FB_COMMIT" -o release/fireboom-mac app/main.go
-CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags "-s -w -X main.FbVersion=$FB_VERSION -X main.FbCommit=$FB_COMMIT" -o release/fireboom-mac-arm64 app/main.go
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w -X main.FbVersion=$FB_VERSION -X main.FbCommit=$FB_COMMIT" -o release/fireboom-linux app/main.go
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "-s -w -X main.FbVersion=$FB_VERSION -X main.FbCommit=$FB_COMMIT" -o release/fireboom-linux-arm64 app/main.go
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-s -w -X main.FbVersion=$FB_VERSION -X main.FbCommit=$FB_COMMIT" -o release/fireboom-windows.exe app/main.go
-CGO_ENABLED=0 GOOS=windows GOARCH=arm64 go build -ldflags "-s -w -X main.FbVersion=$FB_VERSION -X main.FbCommit=$FB_COMMIT" -o release/fireboom-windows-arm64.exe app/main.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w -X main.FbVersion=$FB_VERSION -X main.FbCommit=$FB_COMMIT" -o release/fireboom-mac$BIN_SUFFIX app/main.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags "-s -w -X main.FbVersion=$FB_VERSION -X main.FbCommit=$FB_COMMIT" -o release/fireboom-mac-arm64$BIN_SUFFIX app/main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w -X main.FbVersion=$FB_VERSION -X main.FbCommit=$FB_COMMIT" -o release/fireboom-linux$BIN_SUFFIX app/main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "-s -w -X main.FbVersion=$FB_VERSION -X main.FbCommit=$FB_COMMIT" -o release/fireboom-linux-arm64$BIN_SUFFIX app/main.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-s -w -X main.FbVersion=$FB_VERSION -X main.FbCommit=$FB_COMMIT" -o release/fireboom-windows$BIN_SUFFIX.exe app/main.go
+CGO_ENABLED=0 GOOS=windows GOARCH=arm64 go build -ldflags "-s -w -X main.FbVersion=$FB_VERSION -X main.FbCommit=$FB_COMMIT" -o release/fireboom-windows-arm64$BIN_SUFFIX.exe app/main.go
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 export FB_COMMIT=$(git rev-parse --short HEAD)
-go build -ldflags "-s -w -X main.FbVersion=$FB_VERSION -X main.FbCommit=$FB_COMMIT" -o release/fireboom app/main.go
+go build -ldflags "-s -w -X main.FbVersion=$FB_VERSION -X main.FbCommit=$FB_COMMIT" -o release/fireboom$BIN_SUFFIX app/main.go
