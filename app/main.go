23922cd9e3f9dee49fdfefc4b906cc1d2164f25b
xufeixiang <95065451+lovestaryouth@users.noreply.github.com>
2025-02-10 16:37:06 +0800

    chore: clear useless code on main.go.

--- app/main.go
+++ app/main.go
@@ -4,7 +4,6 @@ import (
 	// Make sure to import swag docs, don't remove
 	_ "fireboom-server/app/docs"
 	"fireboom-server/cmd"
-	"os"
 )
 
 var (
@@ -13,13 +12,5 @@ var (
 )
 
 func main() {
-	// _ = os.Chdir("/path/to/your/project")
-	// _ = os.Chdir("/Users/xufeixiang/IdeaProjects/backend_face_recognition_boarding")
-	// _ = os.Chdir("/Users/xufeixiang/IdeaProjects/case-freetalk")
-	// _ = os.Chdir("/Users/xufeixiang/IdeaProjects/amis-admin/backend")
-	_ = os.Chdir("/Users/xufeixiang/IdeaProjects/turing_ant/turing_ant_backend")
-	// _ = os.Chdir("/Users/xufeixiang/IdeaProjects/dajiache-backend/backend")
-	// _ = os.Chdir("/Users/xufeixiang/IdeaProjects/dianshang_5l")
-	// _ = os.Chdir("/Users/xufeixiang/IdeaProjects/init-todo")
 	cmd.Execute(FbVersion, FbCommit)
 }
